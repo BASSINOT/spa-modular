cddcbf1746bff44f2d90de91442b34b9aa26f7e2
professeurX <xavier.bassinot@gmail.com>
2017-07-01 03:44:59 +0200

    mise en place du systeme de SPA

---
+++ js/spaRooter.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+    
+    if($('.view-name-index').length == 0){
+        alert('Attentnion le systeme requiere une vue indexe');
+    }
+    $('.spaRooter-view').css({
+        left : '100%'
+    });
+    $('.view-name-index').css({
+        left : '0%'
+    });
+    
+    $('.spaRooter-link').click(function(e){
+        e.preventDefault();
+        $view = $(this).attr('href');
+        
+        views = $('.view-name-'+$view);
+        
+        if(views.length==0){
+            alert('erreur 404');
+        }else{
+            $('.spaRooter-view').css({
+                left : '100%'
+            });
+            $('.view-name-'+$view).css({
+                left : '0%'
+            });
+        }
+        
+        
+    });
+});
