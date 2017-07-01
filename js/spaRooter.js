e1a380ce4115987e4d3c95ff42934838ed52d1cc
professeurX <xavier.bassinot@gmail.com>
2017-07-01 03:53:34 +0200

    ajout de la classe active sur les menus

--- js/spaRooter.js
+++ js/spaRooter.js
@@ -2,17 +2,20 @@ $(document).ready(function(){
     
     if($('.view-name-index').length == 0){
         alert('Attentnion le systeme requiere une vue indexe');
+    }else{
+        $('.spaRooter-view').css({
+            left : '100%'
+        });
+        $('.view-name-index').css({
+            left : '0%'
+        });
     }
-    $('.spaRooter-view').css({
-        left : '100%'
-    });
-    $('.view-name-index').css({
-        left : '0%'
-    });
     
     $('.spaRooter-link').click(function(e){
         e.preventDefault();
         $view = $(this).attr('href');
+        $('.spaRooter-link').removeClass('spaActive');
+        $(this).addClass('spaActive');
         
         views = $('.view-name-'+$view);
         
