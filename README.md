4a2badaad3adff7a7098614d5d439849c4da39b0
professeurX <xavier.bassinot@gmail.com>
2017-07-01 13:50:01 +0200

    ajout de la sidebar et du local storage

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
-# SPA-modular
-a simple javascript rooter for SPA
+## SPA-modular
+*a simple javascript rooter for SPA*
+
+How to use ?
+============
+
+First, you have to put on your server the `spaRooter.js` file. Now you can use the SPA-modular html class.
+create view :
+
+    <seciton class="spaRooter-view view-name-index">
+	    //YOUR HTML HERE ..
+    </section>
+use the `spaRooter-view` className to signify to the API there is an view of the SPA.
+
+use the `view-name-XXXXXXX` className to signify to the API there is a name of this view. Yu can replace the XXXXXX for youre view name. WARNING ! Your application can content view-name-index view to use because if you don't have this view the system can't working. The view-name-index class il the main view of youre Single Page application.
+
+to make a link : 
+
+    <a href="page1" class="spaRooter-link">LABEL</a>
+
+all `<a>` there have the spaRooter-link class are used by the API to change view curent. the href attribute if the name of the linked view. 
+for an exemple :
+
+    <a href="myFuturView" class="spaRooter-link">MYFURUTVIEW</a>
+
+This code load the view : view-name-myFuturView
--- js/spaRooter.js
+++ js/spaRooter.js
@@ -1,5 +1,6 @@
 $(document).ready(function(){
     
+    //Vérification de la présence de la page index
     if($('.view-name-index').length == 0){
         alert('Attentnion le systeme requiere une vue indexe');
     }else{
@@ -11,6 +12,7 @@ $(document).ready(function(){
         });
     }
     
+    //gestion des lien quant on clique
     $('.spaRooter-link').click(function(e){
         e.preventDefault();
         $view = $(this).attr('href');
@@ -29,7 +31,64 @@ $(document).ready(function(){
                 left : '0%'
             });
         }
-        
-        
     });
+    //gestion de la side bar
+    $('.menuBtn').addClass('menuClose');
+    $('.spaSideMenu').addClass('spaMenuHidden');
+    $('.menuBtn').click(function(e){
+        e.preventDefault();
+        $(this).css({
+            rotate : '90deg'
+        })
+        elemMenu = $(this);
+        if($('.spaSideMenu').attr('class').replace('spaSideMenu','').replace(' ','')=='spaMenuHidden'){
+            $('.spaSideMenu').removeClass('spaMenuHidden');
+            $(elemMenu).addClass('menuOpen');
+            $(elemMenu).removeClass('menuClose');
+        }else{
+            $('.spaSideMenu').addClass('spaMenuHidden');
+            $(elemMenu).removeClass('menuOpen');
+            $(elemMenu).addClass('menuClose');
+        }
+    });
+    
+    //Gestion du localStorage
+    dataStorage = {}//objet de storage des data de la spi
+    dataStorage.spaHttpAdresse = window.location.href;
+    localStorage
+    
+    //vérification de l'existance du localStorage
+    if(localStorage['formDatas']){
+        $forsDatas = JSON.parse(localStorage['formDatas']);
+        if($forsDatas.spaHttpAdresse == dataStorage.spaHttpAdresse){
+            console.log($forsDatas);
+            $.each($forsDatas,function(n,e){
+                if(n!='spaHttpAdresse'){
+                    $('input[name='+n+']').val(e);
+                    dataStorage[n]=e;
+                }
+            });
+        }
+    }
+    
+    //icrémentation du local storage
+    $('.spa-datas-field').keyup(function(){
+        dataStorage[$(this).attr('name')] = $(this).val();
+        localStorage.setItem('formDatas',JSON.stringify(dataStorage));
+        console.log(dataStorage);
+    });
+    
+    //désactivation de la fonction d'envoie native du form
+    $('form').submit(function(e){
+        e.preventDefault();
+    });
+    
+    
+    //nouvelle fonction submit de la SPA
+    $('form input[type=submit]').click(function(e){
+        e.preventDefault();
+        alert('action de submit');
+        // VOTRE CODE ICI
+    });
+    
 });
